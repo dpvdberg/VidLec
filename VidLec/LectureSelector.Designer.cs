57acd4cf159dc46f8970b5724f81f2d7c4d49e89
Dennis van den Berg <dennis-berg@live.nl>
2016-02-15 20:36:52 +0100

    Basic form

---
+++ VidLec/LectureSelector.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace VidLec
+{
+    partial class LectureSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TLPSplit = new System.Windows.Forms.TableLayoutPanel();
+            this.olvLectures = new BrightIdeasSoftware.ObjectListView();
+            this.olvPresClmName = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvPresClmDate = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvPresClmDuration = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvPresClmViews = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvPresClmDesc = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.tlvAll = new BrightIdeasSoftware.TreeListView();
+            this.tlvAllClmName = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.tlvAllClmCount = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TLPSplit.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.olvLectures)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tlvAll)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TLPSplit
+            // 
+            this.TLPSplit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TLPSplit.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.TLPSplit.ColumnCount = 2;
+            this.TLPSplit.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPSplit.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPSplit.Controls.Add(this.olvLectures, 1, 0);
+            this.TLPSplit.Controls.Add(this.tlvAll, 0, 0);
+            this.TLPSplit.Location = new System.Drawing.Point(12, 27);
+            this.TLPSplit.Name = "TLPSplit";
+            this.TLPSplit.RowCount = 1;
+            this.TLPSplit.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPSplit.Size = new System.Drawing.Size(838, 342);
+            this.TLPSplit.TabIndex = 0;
+            // 
+            // olvLectures
+            // 
+            this.olvLectures.AllColumns.Add(this.olvPresClmName);
+            this.olvLectures.AllColumns.Add(this.olvPresClmDate);
+            this.olvLectures.AllColumns.Add(this.olvPresClmDuration);
+            this.olvLectures.AllColumns.Add(this.olvPresClmViews);
+            this.olvLectures.AllColumns.Add(this.olvPresClmDesc);
+            this.olvLectures.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.olvLectures.CellEditUseWholeCell = false;
+            this.olvLectures.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvPresClmName,
+            this.olvPresClmDate,
+            this.olvPresClmDuration,
+            this.olvPresClmViews,
+            this.olvPresClmDesc});
+            this.olvLectures.Cursor = System.Windows.Forms.Cursors.Default;
+            this.olvLectures.HighlightBackgroundColor = System.Drawing.Color.Empty;
+            this.olvLectures.HighlightForegroundColor = System.Drawing.Color.Empty;
+            this.olvLectures.Location = new System.Drawing.Point(422, 4);
+            this.olvLectures.Name = "olvLectures";
+            this.olvLectures.Size = new System.Drawing.Size(412, 334);
+            this.olvLectures.TabIndex = 1;
+            this.olvLectures.UseCompatibleStateImageBehavior = false;
+            this.olvLectures.View = System.Windows.Forms.View.Details;
+            // 
+            // olvPresClmName
+            // 
+            this.olvPresClmName.AspectName = "Name";
+            this.olvPresClmName.Text = "Name";
+            // 
+            // olvPresClmDate
+            // 
+            this.olvPresClmDate.AspectName = "FullStartDate";
+            this.olvPresClmDate.Text = "Date";
+            this.olvPresClmDate.Width = 68;
+            // 
+            // olvPresClmDuration
+            // 
+            this.olvPresClmDuration.AspectName = "DurationDisplay";
+            this.olvPresClmDuration.Text = "Duration";
+            this.olvPresClmDuration.Width = 113;
+            // 
+            // olvPresClmViews
+            // 
+            this.olvPresClmViews.AspectName = "Views";
+            this.olvPresClmViews.Text = "Views";
+            // 
+            // olvPresClmDesc
+            // 
+            this.olvPresClmDesc.AspectName = "Description";
+            this.olvPresClmDesc.Text = "Description";
+            this.olvPresClmDesc.Width = 108;
+            // 
+            // tlvAll
+            // 
+            this.tlvAll.AllColumns.Add(this.tlvAllClmName);
+            this.tlvAll.AllColumns.Add(this.tlvAllClmCount);
+            this.tlvAll.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlvAll.CellEditUseWholeCell = false;
+            this.tlvAll.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.tlvAllClmName,
+            this.tlvAllClmCount});
+            this.tlvAll.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tlvAll.HighlightBackgroundColor = System.Drawing.Color.Empty;
+            this.tlvAll.HighlightForegroundColor = System.Drawing.Color.Empty;
+            this.tlvAll.Location = new System.Drawing.Point(4, 4);
+            this.tlvAll.Name = "tlvAll";
+            this.tlvAll.ShowGroups = false;
+            this.tlvAll.Size = new System.Drawing.Size(411, 334);
+            this.tlvAll.TabIndex = 2;
+            this.tlvAll.UseCompatibleStateImageBehavior = false;
+            this.tlvAll.View = System.Windows.Forms.View.Details;
+            this.tlvAll.VirtualMode = true;
+            // 
+            // tlvAllClmName
+            // 
+            this.tlvAllClmName.AspectName = "Name";
+            this.tlvAllClmName.Text = "Name";
+            this.tlvAllClmName.Width = 280;
+            // 
+            // tlvAllClmCount
+            // 
+            this.tlvAllClmCount.AspectName = "Count";
+            this.tlvAllClmCount.Text = "Item count";
+            this.tlvAllClmCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tlvAllClmCount.Width = 77;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(12, 372);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(119, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(862, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // LectureSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 394);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.TLPSplit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "LectureSelector";
+            this.Text = "LectureSelector";
+            this.TLPSplit.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.olvLectures)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tlvAll)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TLPSplit;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private BrightIdeasSoftware.ObjectListView olvLectures;
+        private BrightIdeasSoftware.OLVColumn olvPresClmName;
+        private BrightIdeasSoftware.OLVColumn olvPresClmDate;
+        private BrightIdeasSoftware.OLVColumn olvPresClmDuration;
+        private BrightIdeasSoftware.OLVColumn olvPresClmViews;
+        private BrightIdeasSoftware.OLVColumn olvPresClmDesc;
+        private BrightIdeasSoftware.TreeListView tlvAll;
+        private BrightIdeasSoftware.OLVColumn tlvAllClmName;
+        private BrightIdeasSoftware.OLVColumn tlvAllClmCount;
+    }
+}
