564d2a6b4fdf18d5c8c19255fdbafc4469eacd88
Dennis van den Berg <dennis-berg@live.nl>
2016-06-08 21:41:06 +0200

    Changed from BitBucket to Github and added missing files

---
+++ VidLec/Presentation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidLec
+{
+    class Presentation
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PresentationRootId { get; set; }
+        public string Description { get; set; }
+        public string AirDateDisplay { get; set; }
+        public string AirTimeDisplay { get; set; }
+        public string FullStartDate { get; set; }
+        public string FullEndDate { get; set; }
+        public string TimeZoneId { get; set; }
+        public string DurationDisplay { get; set; }
+        public string PlayerUrl { get; set; }
+        public string ModerateUrl { get; set; }
+        public string SlideFormatUrl { get; set; }
+        public string ThumbnailImageUrl { get; set; }
+        public string PresenterImageUrl { get; set; }
+        public string PresentationContentUploadUrl { get; set; }
+        public string PresenterContentUploadUrl { get; set; }
+        public string CardImageIsThumbnail { get; set; }
+        public string StatusDisplay { get; set; }
+        public string Status { get; set; }
+        public string IsShortcut { get; set; }
+        public string IsViewableOnDemand { get; set; }
+        public string IsLive { get; set; }
+        public string IsOnDemand { get; set; }
+        public string SlideCount { get; set; }
+        public string FolderId { get; set; }
+        public string[] SupportingLinks { get; set; }
+        public string[] Tags { get; set; }
+        public string IsEditable { get; set; }
+        public string IsPublishable { get; set; }
+        public string CanModerate { get; set; }
+        public string AllowPresentationDownload { get; set; }
+        public string Available { get; set; }
+        public string TimelineHits { get; set; }
+        public int Views { get; set; }
+
+        public List<Presenter> presenters { get; set; }
+    }
+}
---
+++ VidLec/Presenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidLec
+{
+    class Presenter
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Suffix { get; set; }
+        public string BioUrl { get; set; }
+        public string Email { get; set; }
+        public string ImageName { get; set; }
+        public string ImageUrl { get; set; }
+        public string AdditionalInfo { get; set; }
+        public string Hash { get; set; }
+        public string ParentResource { get; set; }
+    }
+}
